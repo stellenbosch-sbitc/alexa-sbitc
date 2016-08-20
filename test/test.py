9c03ca83b25516f53681aea0ec0e3f210aca43b5
Dylan <20664079@sun.ac.za>
2016-08-20 23:54:23 +0200

    Test with multiple intents

--- test/test.py
+++ test/test.py
@@ -5,20 +5,42 @@ urls = (
     '/alexa', 'index'
 )
 
-class index:
-    def POST(self):
-	data = json.loads(web.data())
-	print data
-        return """
+preferences = {"Dylan" : "None please",
+	"Lisa" : "Two please", 
+	"Sarah" : "One please",
+	"Trandon" : "I don't drink coffee"}
+
+def build_response(text):
+	return """
 	{"response": {
          "outputSpeech": {
          "type": "PlainText",
-         "text": "We can't reach it Sarah"
+         "text": " """ + text + """ "
          },
          "shouldEndSession": true
        }
 } 
 """
+
+class index:
+    def POST(self):
+		data = json.loads(web.data())
+		print data["request"]["type"] == "IntentRequest"
+		if data["request"]["type"] == "IntentRequest":
+			intent = data["request"]["intent"]["name"]
+			print intent
+			if intent == "Speak":
+				return build_response("Hello")
+			elif intent == "Sugar":
+				name = data["request"]["intent"]["slots"]["name"]["value"]
+				if name in preferences:
+					return build_response(preferences[name])
+				else:
+					return build_response("I do not know who that is.")
+			elif intent == "Beauty":
+				return build_response("Beauty comes from within")
+		return build_response("")
+	
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
