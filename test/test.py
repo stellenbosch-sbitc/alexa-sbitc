8f3790507806ba4fdfcf426ecbe6b1b458470310
Dylan <20664079@sun.ac.za>
2016-08-20 18:59:27 +0200

    Test Application

---
+++ test/test.py
@@ -0,0 +1,27 @@
+import web
+import json
+
+urls = (
+    '/alexa', 'index'
+)
+
+class index:
+    def POST(self):
+	data = json.loads(web.data())
+	print data
+        return """
+	{"response": {
+         "outputSpeech": {
+         "type": "PlainText",
+         "text": "Hello"
+         },
+         "shouldEndSession": true
+       }
+} 
+"""
+
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    app.run()
+
+
