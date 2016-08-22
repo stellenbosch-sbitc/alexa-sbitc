09460009247fa8a604e43f5a4655476fd0354a7b
Trandon <Trandonnarasimulu@gmail.com>
2016-08-22 03:51:11 +0200

    To determine the nearby places

---
+++ router/places.py
@@ -0,0 +1,32 @@
+import coordinate
+import requests
+
+def find_nearby_places(address):
+	my_api = 'AIzaSyBtqFCeJlUQAE8Y9zY2grfOXmFouGcJZGI'
+	coords = coordinate.get(address)
+	lat = coords[0]
+	lng = coords[1]
+	placeType = 'restaurant'
+	url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={0},{1}&radius=500&type={2}&key={3}'.format(lat,lng,placeType,my_api)
+	response = requests.get(url)
+	resp_json_payload = response.json()
+	place_coords = []
+	
+	name_of_shop=[] #for the name of the shop
+	for i in range(0,5):
+		latitude = resp_json_payload['results'][i]['geometry']['location']['lat']
+		longitude = resp_json_payload['results'][i]['geometry']['location']['lng']
+		place_coords.append([latitude,longitude])
+		name_of_shop.append(resp_json_payload['results'][i]['name'])
+	
+	locations = [] #address of the shops respectively
+	for k in range(0,len(place_coords)):
+		places = ""
+		new_url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng={0},{1}&key=AIzaSyC9ZPnhBLoe4SZmkghgQKUJrDKtm2Cu-iE'.format(place_coords[k][0],place_coords[k][1])
+		new_response = requests.get(new_url)
+		new_resp = new_response.json();
+		for j in range(0,3):
+			places = places + " " + new_resp['results'][0]['address_components'][j]['long_name']
+		locations.append(places)
+	return name_of_shop, locations
+			
